ae4f4edbf389a6c2b0511feb21108ac8152c77cd
Sunxiaoxue <2844967181@qq.com>
2024-05-15 17:43:08 +0800

    first commit

---
+++ src/main/java/at/guigu/com/Gittest2.java
@@ -0,0 +1,11 @@
+package at.guigu.com;
+
+/**
+ * @Author: smallc
+ * @Date2024/5/1517:32
+ */
+public class Gittest2 {
+    public static void main(String[] args) {
+        System.out.println("git hello");
+    }
+}
